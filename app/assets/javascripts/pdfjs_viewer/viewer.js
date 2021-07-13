ceedc4bea737abfc20beea96ca3ade409d53f14c
Mike Ackerman <mikeackerman@gmail.com>
2021-07-13 16:41:30 -0500

    update HOSTED_VIEWER_ORIGINS allowed array

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -1886,7 +1886,7 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
+    var HOSTED_VIEWER_ORIGINS = ['null', 'https://people-speak-staging.s3.us-west-1.amazonaws.com'];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
       return;
